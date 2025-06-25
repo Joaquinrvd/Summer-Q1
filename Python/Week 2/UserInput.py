5061d1b907d59acf2fc4b71c2e4c9a48511ced70
Joaquin VincentDaGrace <joaquin.vincent-dagrace@hgs.hiddengeniusproject.org>
2025-06-25 14:29:53 -0700

    commit

--- Python/Week 2/UserInput.py
+++ Python/Week 2/UserInput.py
@@ -4,3 +4,6 @@ age = input("cool name, im 14 how about you?")
 print("Your " + age+ "? Thats so cool!")
 grade = input("what grade are you in. Im a freshmen.")
 print("your a " + grade+ " thats so cool!!!")
+#for this do not put any spaces just single word answers
+if grade not in ("freshman", "sophomore", "junior", "senior"):
+    print("Wow, you're in " + grade + "? That's so cool!")
