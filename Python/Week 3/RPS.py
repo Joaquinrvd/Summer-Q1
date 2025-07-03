7d7cf2cfd59ed8ff7e5e588805305903c03e017d
Joaquin VincentDaGrace <joaquin.vincent-dagrace@hgs.hiddengeniusproject.org>
2025-07-03 13:21:31 -0700

    commit

---
+++ Python/Week 3/RPS.py
@@ -0,0 +1,6 @@
+def RPS():
+    print("welcome to Rock Parer Scissors!")
+    player1 = input("Player 1, Please enter your name: ")
+    player2 = input("Player 2, Please enter your name: ")
+
+RPS()
