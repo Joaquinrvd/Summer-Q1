6957856722d8693ebbbe8fae5d05dd0deee6eb0e
Joaquin VincentDaGrace <joaquin.vincent-dagrace@hgs.hiddengeniusproject.org>
2025-07-10 14:29:20 -0700

    Data

---
+++ Python/Week 4/DataScenarios.py
@@ -0,0 +1,47 @@
+# data structures: tuple, list, dictionary, Sets
+#1. A restaurant menu with prices for each item
+print("Scenario #1: A restaurant menu with prices for each item. ")
+print("Best Structure: Dictionary; best way to prices with the items. ")
+menu ={
+    "French Toast": 12,
+    "Grand Slam": 12,
+    "T-Bone": 18,
+    "Avacado Toast": 15
+}
+for key, item in menu.items():
+    print(key, ": ", item)
+
+#2. High scores to an arcade game
+print("\nScenario #2 High scores to an arcade game. ")
+print("Best Structure: List; every time the high score gets broken it gets added")
+highScores = [
+    100,
+    105,
+    110,
+    99
+]
+for score in highScores:
+    print(score)
+
+#3. All of the months of the year
+print("\nAll of the months of the year")
+print("Best structure: Tuple; months are fixed and unchangeable" )
+months = (
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+)
+
+#4. All the items in your backpack
+
+
+#5. Look up Student emails by their names
+
+
+#6. A shopping cart of groceries 
+
+
+#7. [Add a scenario of your own]
+
