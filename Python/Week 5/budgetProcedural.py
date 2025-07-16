22607de8a2c9bce554db125d136edb0c2aa0000a
Joaquin VincentDaGrace <joaquin.vincent-dagrace@hgs.hiddengeniusproject.org>
2025-07-16 13:47:05 -0700

    budget

--- Python/Week 5/budgetProcedural.py
+++ Python/Week 5/budgetProcedural.py
@@ -22,23 +22,33 @@ def Spend(name, amount):
     return budgeted - spent
 
 def PrintBudget():
+    print("  Budget             budgeted     Spent     Remaining")
+    print("-------------------------------------------------------")
+    totalBudgeted = 0
+    totalSpent = 0
+    totalRemaining = 0
     for name in budgets:
         budgeted = budgets[name]
         spent = expenses[name]
         remainingBuget = budgeted
         print(f'{name:15s}, {budgeted:10.2f}, {spent:10.2f}, '
                f'{remainingBuget:10.2f}')
+        totalBudgeted += budgeted
+        totalSpent += spent
+        totalRemaining += remainingBuget
+    print(f'{"Total":15}, {totalBudgeted:10.2f}, {totalSpent:10.2f}, '
+          f'{totalBudgeted - totalSpent:10.2f}')
 
 
 print("Total Funds: ", funds)
 addbudget("Games", 60)
 addbudget("Better PC", 1000)
 addbudget("Crunchyroll+", 10)
-addbudget("Rent (Just sleep on the street as long as you have games)", 0)
+addbudget("Rent(Homeless)", 0)
 
 Spend("Games", 60)
 Spend("Better PC", 200)
 Spend("Crunchyroll+", 10)
-Spend("Rent (Just sleep on the street as long as you have games)", 0)
+Spend("Rent(Homeless)", 0)
 
 PrintBudget()
